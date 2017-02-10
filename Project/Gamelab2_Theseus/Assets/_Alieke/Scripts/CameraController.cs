82fb79fb1db90fad9714febceffde5c5fdf1b510
Alieke <aliekevanderlelie@hotmail.com>
2017-02-10 14:45:22 +0100

    Player movement and camera controller in my scene

---
+++ Project/Gamelab2_Theseus/Assets/_Alieke/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    Vector2 mouseChange;
+    Vector2 smoothAmount;
+    public float mouseSensitivity;
+    public float smoothing;
+
+
+	void FixedUpdate () {
+        MoveCamera();
+	}
+
+    void MoveCamera()
+    {
+        Vector2 mouseMovement = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        mouseMovement = Vector2.Scale(mouseMovement,new Vector2(mouseSensitivity * smoothing, mouseSensitivity * smoothing));
+       
+        smoothAmount.x = Mathf.Lerp(smoothAmount.x, mouseMovement.x, 1f / smoothing);
+        smoothAmount.y = Mathf.Lerp(smoothAmount.y, mouseMovement.y, 1f / smoothing);
+        
+        mouseChange += smoothAmount;
+        mouseChange.y = Mathf.Clamp(mouseChange.y, -65, 65);
+
+        transform.localRotation = Quaternion.AngleAxis(-mouseChange.y, Vector3.right);
+        player.transform.localRotation = Quaternion.AngleAxis(mouseChange.x, player.transform.up);
+    }
+}
---
+++ Project/Gamelab2_Theseus/Assets/_Alieke/Scripts/PlayerController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public GameObject gameManager;
+    enum States {Idle, Walking}
+    States movingStates;
+
+    public float moveSpeed;
+    public float accelerationSpeed;
+    public float maxSpeed;
+    private float normalSpeed;
+
+    public float heightRayDis;
+
+    private Rigidbody _rb;
+    private Animator _Anim;
+    public float jumpForce;
+    private bool mayJump = true;
+
+
+	void Awake ()
+    {
+        normalSpeed = moveSpeed;
+        _rb = GetComponent<Rigidbody>();
+        _Anim = gameManager.GetComponent<Animator>();
+	}
+	
+	void FixedUpdate ()
+    {
+        Movement();
+        Jump();
+
+        switch (movingStates)
+        {
+            case States.Idle:
+                _Anim.SetBool("Walking", false);
+                break;
+            case States.Walking:
+                _Anim.SetBool("Walking", true);
+                break;
+        }
+    }
+
+    void Movement()
+    {
+         float xInput = Input.GetAxis("Horizontal");
+         float yInput = Input.GetAxis("Vertical");
+        _rb.MovePosition(transform.position + transform.forward * Time.deltaTime * moveSpeed * yInput +  transform.right * Time.deltaTime * moveSpeed * xInput);
+        if (yInput > 0)
+        {
+            movingStates = States.Walking;
+            if (Input.GetButton("Shift"))
+            {
+                moveSpeed += accelerationSpeed * Time.deltaTime;
+                Mathf.Clamp(moveSpeed, 0, maxSpeed);
+            }
+        }
+        else if (moveSpeed > normalSpeed)
+        {
+            moveSpeed -= accelerationSpeed * Time.deltaTime;
+        }
+        else
+        {
+            movingStates = States.Idle;
+        }
+        if (Physics.Raycast(transform.position, -transform.up, heightRayDis))
+        {
+            _rb.useGravity = false;
+        }
+        else
+        {
+            _rb.useGravity = true;
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        { 
+            if (mayJump)
+            {
+                _rb.AddForce(Vector3.up * jumpForce);
+                mayJump = false;
+            }
+        }
+        if(Physics.Raycast(transform.position,-transform.up, heightRayDis))
+        {
+            mayJump = true;
+        }
+    }
+}
