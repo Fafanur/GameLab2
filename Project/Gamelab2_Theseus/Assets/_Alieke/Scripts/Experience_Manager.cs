edc22c0fd82e39bfbf08b7b6df1c672f538ad6f1
Alieke <aliekevanderlelie@hotmail.com>
2017-02-11 16:46:00 +0100

    Assetlist update. Player controller almost done and experience manager as good as done

---
+++ Project/Gamelab2_Theseus/Assets/_Alieke/Scripts/Experience_Manager.cs
@@ -0,0 +1,102 @@
+﻿//made by Alieke
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Experience_Manager : MonoBehaviour {
+    public PlayerController player;
+    public Statistics stats;
+
+    public float currentExperience;
+    public float neededExperience;
+
+    public int currentLevel;
+    public int experiencePoints;
+
+
+	void Awake () {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+	}
+	
+
+	void Update () {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            currentExperience = currentExperience + 10;
+            GotExperience();
+        }
+	}
+
+    public void GotExperience()
+    {
+        if(currentExperience > neededExperience)
+        {
+            currentLevel++;
+            experiencePoints++;
+            currentExperience = currentExperience - neededExperience;
+            neededExperience = neededExperience * 1.15f;
+            neededExperience = Mathf.Round(neededExperience);
+            currentExperience =  Mathf.Round(currentExperience);
+        }
+    }
+
+    public void AddStrength()
+    {
+        if (experiencePoints > 0)
+        {
+            stats.strength++;
+            experiencePoints--;
+            player.attack.attackDamage *= 1.25f;
+            player.attack.critDamage +=0.5f;
+        }
+    }
+
+    public void AddCrit()
+    {
+        if (experiencePoints > 0)
+        {
+            stats.critChance++;
+            experiencePoints--;
+            player.attack.critChance += 0.25f;
+        }
+    }
+
+    public void AddStamina()
+    {
+        if (experiencePoints > 0)
+        {
+            stats.stamina++;
+            experiencePoints--;
+            player.stamina.maxStamina = Mathf.Round(player.stamina.maxStamina * 1.12f);         
+        }
+    }
+
+    public void AddHealth()
+    {
+        if (experiencePoints > 0)
+        {
+            stats.health++;
+            experiencePoints--;
+            player.health.maxHealth = Mathf.Round(player.health.maxHealth * 1.05f);
+        }
+    }
+
+    public void AddDefense()
+    {
+        if (experiencePoints > 0)
+        {
+            stats.defense++;
+            experiencePoints--;
+            player.defense.defenseAmount += 0.15f;
+            player.defense.blockChance += 0.1f;
+        }
+    }
+}
+
+
+
+[System.Serializable]
+public class Statistics
+{
+    public float strength, stamina, health, defense, critChance;
+}
--- Project/Gamelab2_Theseus/Assets/_Alieke/Scripts/PlayerController.cs
+++ Project/Gamelab2_Theseus/Assets/_Alieke/Scripts/PlayerController.cs
@@ -1,33 +1,59 @@
-﻿using System.Collections;
+﻿//made by Alieke
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+    //Components
     public GameObject gameManager;
-    enum States {Idle, Walking}
-    States movingStates;
-
-    public float moveSpeed;
-    public float accelerationSpeed;
-    public float maxSpeed;
-    private float normalSpeed;
-
-    public float heightRayDis;
-
     private Rigidbody _rb;
     private Animator _Anim;
-    public float jumpForce;
+    public GameObject statPanel;
+
+    enum States { Idle, Walking }
+    States movingStates;
+
+
+    public Movement movement;
+    public Stamina stamina;
+    public Health health;
+    public Attack attack;
+    public Defense defense;
+
     private bool mayJump = true;
+    private bool mayAttack = true;
+
+    private bool statPanelOpen;
 
 
-	void Awake ()
+    void Awake()
     {
-        normalSpeed = moveSpeed;
+        stamina.currentStamina = stamina.maxStamina;
+        movement.normalSpeed = movement.moveSpeed;
         _rb = GetComponent<Rigidbody>();
         _Anim = gameManager.GetComponent<Animator>();
-	}
-	
-	void FixedUpdate ()
+    }
+
+    void Update() // Opens the statistics panel
+    {
+        if (Input.GetButtonDown("Open Stats"))
+        {
+            if (statPanelOpen)
+            {
+                statPanel.SetActive(false);
+                statPanelOpen = false;
+                mayAttack = true;
+            }
+            else
+            {
+                statPanel.SetActive(true);
+                mayAttack = false;
+                statPanelOpen = true;
+            }
+        }
+    }
+
+    void FixedUpdate()
     {
         Movement();
         Jump();
@@ -45,27 +71,37 @@ public class PlayerController : MonoBehaviour {
 
     void Movement()
     {
-         float xInput = Input.GetAxis("Horizontal");
-         float yInput = Input.GetAxis("Vertical");
-        _rb.MovePosition(transform.position + transform.forward * Time.deltaTime * moveSpeed * yInput +  transform.right * Time.deltaTime * moveSpeed * xInput);
+        float xInput = Input.GetAxis("Horizontal");
+        float yInput = Input.GetAxis("Vertical");
+        _rb.MovePosition(transform.position + transform.forward * Time.deltaTime * movement.moveSpeed * yInput + transform.right * Time.deltaTime * movement.moveSpeed * xInput);
         if (yInput > 0)
         {
             movingStates = States.Walking;
             if (Input.GetButton("Shift"))
             {
-                moveSpeed += accelerationSpeed * Time.deltaTime;
-                Mathf.Clamp(moveSpeed, 0, maxSpeed);
+                if (stamina.currentStamina > 0)
+                {
+                    movement.moveSpeed += movement.accelerationSpeed * Time.deltaTime;
+                    Mathf.Clamp(movement.moveSpeed, 0, movement.maxSpeed);
+                    stamina.currentStamina -= stamina.staminaDrain * Time.deltaTime;
+                }
             }
         }
-        else if (moveSpeed > normalSpeed)
+        else if (movement.moveSpeed > movement.normalSpeed)
         {
-            moveSpeed -= accelerationSpeed * Time.deltaTime;
+            movement.moveSpeed -= movement.accelerationSpeed * Time.deltaTime;
         }
         else
         {
             movingStates = States.Idle;
         }
-        if (Physics.Raycast(transform.position, -transform.up, heightRayDis))
+
+        if (stamina.currentStamina < stamina.maxStamina && !Input.GetButtonDown("Shift"))
+        {
+            stamina.currentStamina += stamina.staminaRecover * Time.deltaTime;
+        }
+
+        if (Physics.Raycast(transform.position, -transform.up, movement.heightRayDis))
         {
             _rb.useGravity = false;
         }
@@ -78,16 +114,108 @@ public class PlayerController : MonoBehaviour {
     void Jump()
     {
         if (Input.GetButtonDown("Jump"))
-        { 
+        {
             if (mayJump)
             {
-                _rb.AddForce(Vector3.up * jumpForce);
+                _rb.AddForce(Vector3.up * movement.jumpForce);
                 mayJump = false;
             }
         }
-        if(Physics.Raycast(transform.position,-transform.up, heightRayDis))
+        if (Physics.Raycast(transform.position, -transform.up, movement.heightRayDis))
         {
             mayJump = true;
         }
     }
+
+    void Attack()
+    {
+        print(CritStrike());
+        if (CritStrike())
+        {
+            attack.totalDamage = Mathf.Round(attack.attackDamage * attack.critDamage);
+            //attack animation
+        }
+    }
+
+    private  bool CritStrike() //Calculates if attack crits
+    {
+        float number = Random.Range(1, 101);
+        if (attack.critChance >= number)
+        {
+            return true;
+         }
+        else
+        {
+            return false;
+        }
+    }
+
+    float GetHit (float damage)
+    {
+        if (!BlockChance())
+        {
+            health.currentHealth -= (damage - defense.defenseAmount);
+            if (health.currentHealth < 0)
+            {
+                //death
+            }
+        }
+        return health.currentHealth;
+    }
+
+    bool BlockChance() // calculate if the player blocks
+    {
+        float number = Random.Range(1, 101);
+        if(number <= defense.blockChance)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    float GetHealth(float healAmount)
+    {
+        if ((health.currentHealth + healAmount) > health.maxHealth){
+            health.currentHealth = health.maxHealth;         
+        }
+        else
+        {
+            health.currentHealth += healAmount;
+        }
+        return health.currentHealth;
+    }
+}
+
+[System.Serializable]
+public class Attack
+{
+    public float attackDamage, critChance,critDamage, totalDamage;
+}
+
+[System.Serializable]
+public class Health
+{
+    public float currentHealth, maxHealth;
+}
+
+[System.Serializable]
+public class Stamina
+{
+    public float currentStamina, maxStamina, staminaDrain, staminaRecover;
+}
+
+[System.Serializable]
+public class Defense
+{
+    public float blockChance, defenseAmount;
+}
+
+[System.Serializable]
+public class Movement
+{
+    public float moveSpeed, accelerationSpeed, maxSpeed, normalSpeed, heightRayDis, jumpForce;
+
 }
