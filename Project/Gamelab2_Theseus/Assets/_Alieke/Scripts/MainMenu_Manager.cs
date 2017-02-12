9f7c54b81cd7104e518b94abe60b38e2b1f3de2b
Alieke <aliekevanderlelie@hotmail.com>
2017-02-12 15:03:11 +0100

    Pause manager and main menu manager

---
+++ Project/Gamelab2_Theseus/Assets/_Alieke/Scripts/MainMenu_Manager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu_Manager : MonoBehaviour {
+    public GameObject mainMenuPanel;
+    public GameObject optionsPanel;
+    public GameObject creditsPanel;
+
+    public void StartGameButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void MainMenuButton()
+    {
+        mainMenuPanel.SetActive(true);
+        optionsPanel.SetActive(false);
+        creditsPanel.SetActive(false);
+    }
+
+    public void OptionsButton()
+    {
+        mainMenuPanel.SetActive(false);
+        optionsPanel.SetActive(true);
+    }
+
+    public void CreditsButton()
+    {
+        mainMenuPanel.SetActive(false);
+        creditsPanel.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Project/Gamelab2_Theseus/Assets/_Alieke/Scripts/Pause_Manager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause_Manager : MonoBehaviour {
+    public bool paused;
+    public GameObject pausePanel;
+    public GameObject optionsPanel;
+	
+	void Update () {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (paused)
+            {
+                Time.timeScale = 1;
+                pausePanel.SetActive(false);
+                paused = false;
+            }
+            else
+            {        
+                Time.timeScale = 0;
+                pausePanel.SetActive(true);
+                paused = true;
+            }
+        }
+	}
+
+    public void ContinueGameButton()
+    {
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+        paused = false;
+    }
+
+    public void PauseMenuButton()
+    {
+        optionsPanel.SetActive(false);
+        pausePanel.SetActive(true);
+    }
+
+    public void OptionsButton()
+    {
+        optionsPanel.SetActive(true);
+        pausePanel.SetActive(false);
+    }
+
+    public void QuitButton()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
